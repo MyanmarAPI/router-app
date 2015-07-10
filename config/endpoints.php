e822524f667e6fc4df7692e0e83923298c6490ef
DragonVirus <limonster.li@gmail.com>
2015-07-10 16:33:01 +0630

    move endpoint base urls to .env for security reason

--- config/endpoints.php
+++ config/endpoints.php
@@ -6,7 +6,7 @@
   | 'endpoint-uri' => [
   | 	'name' => 'Name of the endpoint',
   |		'desc' => 'Description of the endpoint',
-  |		'base' => 'http://endpoint-base-url.com/',
+  |		'base' => 'http://endpoint-base-url.com/candidate/v1/', //Inculde "/" at the end of base_url
   |		'docs' => 'http://endpoint-docs-url.com',
   |		'API_KEYS' => 'SomeRandomKey', //Leave it blank if your endpoint isn't filter with these keys.
   |		'API_SECRET' => 'SomeRandomSecret',
@@ -15,12 +15,12 @@
  */
 
 return [
-	'candidates' => [
+	'candidate' => [
 		'name' => 'Candidate API',
 		'desc' => '',
-		'base' => '',
+		'base' => env('CANDIDATES_BASE'),
 		'docs' => '',
-		'API_KEYS' => env('CANDIDATES_KEY'),
+		'API_KEY' => env('CANDIDATES_KEY'),
 		'API_SECRET' => env('CANDIDATES_SECRET')
 	],
 	'faq' => [
@@ -42,7 +42,7 @@ return [
 	'party' => [
 		'name' => 'Party API',
 		'desc' => 'Return Party List On Myanmar Election',
-		'base' => 'http://10.240.207.246:8080/',
+		'base' => env('PARTY_BASE'),
 		'docs' => '',
 		'API_KEY' => '', 
 		'API_SECRET' => ''
