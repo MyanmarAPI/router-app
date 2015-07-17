b67ab1bbaef09ebf0301ad3172c65310f1f80302
DragonVirus <limonster.li@gmail.com>
2015-07-17 18:03:08 +0630

    Update CORSMiddleware

--- app/Http/Middleware/CORSMiddleware.php
+++ app/Http/Middleware/CORSMiddleware.php
@@ -3,18 +3,32 @@
 namespace App\Http\Middleware;
 
 use Closure;
+use Response;
 
 class CORSMiddleware
 {
     public function handle($request, Closure $next)
     {
+
+    	$headers = [
+    		'Access-Control-Allow-Origin' => '*',
+    		'Access-Control-Allow-Headers' => 'X-API-KEY, X-API-SECRET',
+    		'Access-Control-Allow-Methods' => 'GET, OPTIONS'
+    	];
+
+    	if($request->getMethod() == "OPTIONS") {
+
+    		return Response::make('OK', 200, $headers);
+
+    	}
+
         $response = $next($request);
 
-        $response->headers->set('Access-Control-Allow-Origin', '*');
+        foreach ($headers as $key => $value) {
 
-        $response->headers->set('Access-Control-Allow-Headers', 'X-API-KEY, X-API-SECRET');
+        	$response->headers->set($key, $value);
 
-        $response->headers->set('Access-Control-Allow-Methods', 'GET');
+        }
 
         return $response;
     }
