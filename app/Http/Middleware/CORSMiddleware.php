4f666bf4f46e868181e7aa6ac52b225865874bab
DragonVirus <limonster.li@gmail.com>
2015-07-17 16:28:32 +0630

    Add space between header keys in CORSMiddleware

--- app/Http/Middleware/CORSMiddleware.php
+++ app/Http/Middleware/CORSMiddleware.php
@@ -12,7 +12,7 @@ class CORSMiddleware
 
         $response->headers->set('Access-Control-Allow-Origin', '*');
 
-        $response->headers->set('Access-Control-Allow-Headers', 'X-API-KEY,X-API-SECRET');
+        $response->headers->set('Access-Control-Allow-Headers', 'X-API-KEY, X-API-SECRET');
 
         $response->headers->set('Access-Control-Allow-Methods', 'GET');
 
