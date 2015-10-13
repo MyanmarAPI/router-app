f13f07061ad70303c01451560ff1630c4ab74652
Nyan Lynn Htut <lynnhtut87@gmail.com>
2015-10-13 18:44:17 +0630

    Update logging

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,6 +1,9 @@
 <?php namespace App\Providers;
 
+use Monolog\Logger;
+use Monolog\Formatter\LineFormatter;
 use Illuminate\Support\ServiceProvider;
+use Monolog\Handler\RotatingFileHandler;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -12,6 +15,24 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->configureLogging($this->app);
+        
     }
+
+    /**
+     * Configure logging with daily log.
+     *
+     * @param  \Illuminate\Contracts\Foundation\Application
+     * @return void
+     */
+    protected function configureLogging($app)
+    {
+        $app->singleton('Psr\Log\LoggerInterface', function () {
+            $handler = new RotatingFileHandler(storage_path('logs/lumen.log'), 3, Logger::DEBUG);
+            $handler->setFormatter(new LineFormatter(null, null, true, true));
+
+            return new Logger('lumen', [$handler]);
+        });
+    }
+
 }
